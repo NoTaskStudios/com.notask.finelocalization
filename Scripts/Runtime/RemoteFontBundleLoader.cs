d4232b82596705ea8b2e2e6f84aeaa56c2e2978d
Valdeci Danilo BSA <valdeci.danilo@bsatech.io>
2026-05-28 12:04:33 -0300

    fix: remove suprime erros and local update check

--- Scripts/Runtime/RemoteFontBundleLoader.cs
+++ Scripts/Runtime/RemoteFontBundleLoader.cs
@@ -126,7 +126,6 @@ namespace FineLocalization.Scripts.Runtime
         private readonly HashSet<TMP_FontAsset> _seenFonts = new();
         private readonly HashSet<TMP_FontAsset> _fontValidationStack = new();
         private bool _ignoreNextLocalizationChanged;
-        private int _preRenderRepairFrames;
 
         public static event Action<bool> OnDownloadRemoteFontComplete = _ => { };
         public static event Action<string, bool> OnRemoteFontDownloadComplete = (_, _) => { };
@@ -137,8 +136,6 @@ namespace FineLocalization.Scripts.Runtime
 
         private void OnEnable()
         {
-            Canvas.preWillRenderCanvases += RepairFallbacksBeforeCanvasRender;
-
             if (loadOnLocalizationChanged)
                 LocalizationManager.OnLocalizationChanged += EnsureCurrentLanguageFont;
 
@@ -157,21 +154,8 @@ namespace FineLocalization.Scripts.Runtime
 
         private void OnDisable()
         {
-            Canvas.preWillRenderCanvases -= RepairFallbacksBeforeCanvasRender;
-
             if (loadOnLocalizationChanged)
                 LocalizationManager.OnLocalizationChanged -= EnsureCurrentLanguageFont;
-        }
-
-        private void RepairFallbacksBeforeCanvasRender()
-        {
-            if (_preRenderRepairFrames <= 0)
-                return;
-
-            _preRenderRepairFrames--;
-            RepairGlobalTMPFallbacks();
-            RepairLoadedFontFallbackTrees();
-            ClearTMPFallbackMaterialCache();
         }
 
         public void EnsureCurrentLanguageFont()
@@ -420,7 +404,6 @@ namespace FineLocalization.Scripts.Runtime
             RepairGlobalTMPFallbacks();
             RepairLoadedFontFallbackTrees();
             ClearTMPFallbackMaterialCache();
-            _preRenderRepairFrames = 8;
             bundle.Unload(false);
             CompleteFontDownload(normalizedLanguage, true, onComplete, true);
         }
